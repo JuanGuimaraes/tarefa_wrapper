8b938eb292f19a7870e7c6b0d86e92ab945c9cbd
JuanGuimaraes <juandasmerces@outlook.com>
2024-09-22 14:58:05 -0300

    first commit

---
+++ src/br/com/juan/Main.java
@@ -0,0 +1,10 @@
+package br.com.juan;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 50;
+        System.out.println(numero);
+        int numero1 = new Integer(50);
+        System.out.println(numero1);
+    }
+}
