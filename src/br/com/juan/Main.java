9f1f68ec9bef06f1fd9b8fd27c3e3e2781805265
JuanGuimaraes <juandasmerces@outlook.com>
2024-09-23 19:40:41 -0300

    corrigindo o exercicio

--- src/br/com/juan/Main.java
+++ src/br/com/juan/Main.java
@@ -2,9 +2,10 @@ package br.com.juan;
 
 public class Main {
     public static void main(String[] args) {
-        int numero = 50;
-        System.out.println(numero);
-        int numero1 = new Integer(50);
-        System.out.println(numero1);
+        int numeroPrimitivo = 50;
+        System.out.println(numeroPrimitivo);
+
+        Integer numeroWrapper = Integer.valueOf(numeroPrimitivo);
+        System.out.println(numeroWrapper);
     }
 }
